12a7249e2e64f2af3372488f8c86cdd8ec92e111
Akram <khalilovakram07@gmail.com>
2024-11-06 19:50:19 +0600

    add new project

---
+++ src/script/script.js
@@ -0,0 +1,8 @@
+// var ключевое слово для создания коробки (переменная) 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    // loop: true
+});
